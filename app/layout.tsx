23503e4ed4c7da0a869c476a9c794223e5ab0665
nazrul-ansari <ansarinazrul99@gmail.com>
2025-10-01 23:11:58 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Navbar />
         <main className="container mx-auto py-12 px-4">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
